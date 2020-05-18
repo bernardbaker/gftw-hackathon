920134da9eec55f7ea081f8ce06dc355dd3ae658
Bibiana Souza <souzabibiana@hotmail.com>
2020-05-17 21:17:44 -0700

    Added a News Page feed with small snippets in a grid format. Have to work a few formatting issues and styling as well.

--- gftw-hackathon/src/index.js
+++ gftw-hackathon/src/index.js
@@ -2,9 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
+import NewsPage from "./pages/landing-page/NewsPage";
 
 ReactDOM.render(
   <React.StrictMode>
+    <NewsPage />
     <App />
   </React.StrictMode>,
   document.getElementById("root")
