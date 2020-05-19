e765960dd7a030cf71e0328a1c6b9a30085a4877
Bernard Baker <bernard.baker.me@gmail.com>
2020-05-19 23:42:01 +0100

    Moved files.

---
+++ src/pages/landing-page/News.js
@@ -0,0 +1,31 @@
+import React from "react";
+import "./News.css";
+
+const News = () => {
+  const importWebComponents = async () => {
+    await import("../../common/NewsArticle");
+  };
+  importWebComponents();
+
+  return (
+    <>
+      <section>
+        <div className="column">
+          <news-article id="eba1cbef76ebe0f945afa196f9fb3027"></news-article>
+          {/* <news-article id="9719373229cdbc235ef88bb4d693d53c"></news-article>
+          <news-article id="eba1cbef76ebe0f945afa196f9fb3027"></news-article>
+          <news-article id="9719373229cdbc235ef88bb4d693d53c"></news-article> */}
+        </div>
+        {/* <div class="column">
+          <news-article id="9719373229cdbc235ef88bb4d693d53c"></news-article>
+        </div>
+        <div class="column">
+          <news-article id="9719373229cdbc235ef88bb4d693d53c"></news-article>
+          <news-article id="eba1cbef76ebe0f945afa196f9fb3027"></news-article>
+        </div> */}
+      </section>
+    </>
+  );
+};
+
+export default News;
