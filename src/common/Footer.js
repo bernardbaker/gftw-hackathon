722929eeda75cb5f78755370fb9735c9738e237b
Bernard Baker <bernard.baker.me@gmail.com>
2020-06-05 15:44:20 +0100

    Cleaned up code. But I'm waiting for BiBi to document and comment the code.

--- src/common/Footer.js
+++ src/common/Footer.js
@@ -5,6 +5,9 @@ const Footer = () => {
   return (
     <footer>
       <a href="https://github.com/bernardbaker/gftw-hackathon">GitHub</a>
+      <a href="https://coil.com">Get Coil</a>
+      <a href="https://dev.to">Join Dev.to</a>
+      <a href="https://www.grantfortheweb.org">Grant For The Web</a>
     </footer>
   );
 };
--- src/pages/landing-page/News.js
+++ src/pages/landing-page/News.js
@@ -12,17 +12,10 @@ const News = () => {
       <section>
         <div className="column">
           <news-article id="eba1cbef76ebe0f945afa196f9fb3027"></news-article>
-          {/* <news-article id="9719373229cdbc235ef88bb4d693d53c"></news-article>
-          <news-article id="eba1cbef76ebe0f945afa196f9fb3027"></news-article>
-          <news-article id="9719373229cdbc235ef88bb4d693d53c"></news-article> */}
-        </div>
-        {/* <div class="column">
-          <news-article id="9719373229cdbc235ef88bb4d693d53c"></news-article>
         </div>
         <div class="column">
-          <news-article id="9719373229cdbc235ef88bb4d693d53c"></news-article>
-          <news-article id="eba1cbef76ebe0f945afa196f9fb3027"></news-article>
-        </div> */}
+          <news-article id="ba9e844de1b0c6f8f8135154a8480d76"></news-article>
+        </div>
       </section>
     </>
   );
