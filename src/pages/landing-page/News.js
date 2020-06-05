591b5ea33e0aaaa2dcb72f7b87336f983ed45974
Bernard Baker <bernard.baker.me@gmail.com>
2020-06-05 19:25:17 +0100

    Fixed ID.

--- src/pages/landing-page/News.js
+++ src/pages/landing-page/News.js
@@ -14,7 +14,7 @@ const News = () => {
           <news-article id="eba1cbef76ebe0f945afa196f9fb3027"></news-article>
         </div>
         <div class="column">
-          <news-article id="9719373229cdbc235ef88bb4d693d53c"></news-article>
+          <news-article id="ba9e844de1b0c6f8f8135154a8480d76"></news-article>
         </div>
         <div class="column">
           <news-article id="ba9e844de1b0c6f8f8135154a8480d76"></news-article>
